580438e92c550649c68810228da6cd1a91c64934
luisgomezlg <luiseduar1997@gmail.com>
2024-02-03 17:38:21 -0500

    Cambios generales

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -55,7 +55,7 @@
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
         <DisclosureButton v-for="(item, index) in navigation" @click="selectItem(index)" :key="item.name" as="a" :href="item.href" 
-        :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+        :class="[item.current ? '' : '', 'block rounded-md px-3 py-2 text-base font-medium', { 'bg-gray-900 text-orange-500 border-orange-500 border-b-4': selectedIndex === index }]"
          :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
       </div>
     </DisclosurePanel>
